59396d9fd48026c54d789d20e432765a2d6747a4
Vicktor007 <vicktord007@gmail.com>
2023-11-15 09:31:21 +0100

    test changes

--- src/components/product/productList/ProductList.jsx
+++ src/components/product/productList/ProductList.jsx
@@ -122,8 +122,8 @@ const ProductList = ({ products, isLoading }) => {
                     
                     <tr key={_id}>
                        <td>{index + 1}</td>
-                      <td>{shortenText(name, 16)}</td>
-                      {/* <td>{name ? shortenText(name, 16) : ''}</td> */}
+                      {/* <td>{shortenText(name, 16)}</td> */}
+                      <td>{name ? shortenText(name, 16) : ''}</td>
                       <td>{category}</td>
                       <td>
                         {"$"}
